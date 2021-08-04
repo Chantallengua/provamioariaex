68ff0fab8cdcbd1ec48ad798faf0532441aec06c
Chantal Lengua <43709607+Chantallengua@users.noreply.github.com>
2021-08-04 21:20:23 +0200

    up

--- src/index.php
+++ src/index.php
@@ -5,11 +5,31 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Accessibility examples</title>
+  
+  <style>
+  body {
+font-size: 18px;
+/* background-color: #fffceb;
+padding: 1em 1em 1em 2em; 
+margin: 1em; */
+}
+
+li {
+padding: 0.25em;
+}
+
+h1 {
+text-align: center;
+font-size: 36px;
+margin-top: 0.5em !important;
+}
+  </style>
+  
 </head>
 <body>
   <main>
-    <h1>Accessibility examples</h1>
-    <p>The good, the bad and the ugly of web accessibility</p>
+    <h1 class="saharian-debug-h1">Accessibility examples</h1>
+    <p>The good, the bad and the ugly of web accessibility.</p>
     <ul><?php
     $examples = glob('*/index.html');
     foreach ($examples as $example) {
